7af53c1cfa5b1d4c5280e0b56eb3293f5a72863d
mohamed00Sayed <sayedm22529@gmail.com>
2021-10-04 19:57:59 +0200

    fix: use xml config only

--- src/main/java/mixed/com/sayed/stereo/soundsys/SoundSystemConfig.java
+++ src/main/java/mixed/com/sayed/stereo/soundsys/SoundSystemConfig.java
@@ -1,14 +1,11 @@
 package mixed.com.sayed.stereo.soundsys;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
-import auto.com.sayed.stereo.soundsys.CDPlayerConfig;
 
 @Configuration
-@Import(CDPlayerConfig.class)
-@ImportResource("classpath:cd-config.xml")
+@ImportResource("classpath*:cd-config.xml")
 public class SoundSystemConfig {
 
 }
