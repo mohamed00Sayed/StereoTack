8a85a6bb12d257cd0bd795dc9e3284120f899398
mohamed00Sayed <sayedm22529@gmail.com>
2021-10-04 20:55:20 +0200

    test: adding tests for xml-based configurations

---
+++ src/test/java/xmlconf/com/sayed/stereo/soundsys/test/PNamespaceWithUtilNamespaceTest.java
@@ -0,0 +1,46 @@
+package xmlconf.com.sayed.stereo.soundsys.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import xmlconf.com.sayed.stereo.soundsys.MediaPlayer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath*:PNamespaceWithUtilNamespaceTest-context.xml")
+public class PNamespaceWithUtilNamespaceTest {
+
+  @Rule
+  public final SystemOutRule log = new SystemOutRule().enableLog();
+
+  @Autowired
+  private MediaPlayer player;
+
+  @Test
+  public void play() {
+    player.play();
+    assertEquals(
+        "Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\n" +
+        "-Track: Sgt. Pepper's Lonely Hearts Club Band\n" +
+        "-Track: With a Little Help from My Friends\n" +
+        "-Track: Lucy in the Sky with Diamonds\n" +
+        "-Track: Getting Better\n" +
+        "-Track: Fixing a Hole\n" +
+        "-Track: She's Leaving Home\n" +
+        "-Track: Being for the Benefit of Mr. Kite!\n" +
+        "-Track: Within You Without You\n" +
+        "-Track: When I'm Sixty-Four\n" +
+        "-Track: Lovely Rita\n" +
+        "-Track: Good Morning Good Morning\n" +
+        "-Track: Sgt. Pepper's Lonely Hearts Club Band (Reprise)\n" +
+        "-Track: A Day in the Life\n",
+        log.getLogWithNormalizedLineSeparator());
+  }
+
+}
---
+++ src/test/java/xmlconf/com/sayed/stereo/soundsys/test/PropertyRefTest.java
@@ -0,0 +1,33 @@
+package xmlconf.com.sayed.stereo.soundsys.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import xmlconf.com.sayed.stereo.soundsys.MediaPlayer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath*:PropertyRefTest-context.xml")
+public class PropertyRefTest {
+
+  @Rule
+  public final SystemOutRule log = new SystemOutRule().enableLog();
+
+  @Autowired
+  private MediaPlayer player;
+
+  @Test
+  public void play() {
+    player.play();
+    assertEquals(
+        "Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\n", 
+        log.getLogWithNormalizedLineSeparator());
+  }
+
+}
