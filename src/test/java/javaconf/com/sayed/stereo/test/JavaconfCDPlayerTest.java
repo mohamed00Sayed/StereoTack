8c4590ed5b45ca01104ed428c5d6948f8e0043dd
mohamed00Sayed <sayedm22529@gmail.com>
2021-10-07 11:44:24 +0200

    test: add test for standalone java configurations

---
+++ src/test/java/javaconf/com/sayed/stereo/test/JavaconfCDPlayerTest.java
@@ -0,0 +1,43 @@
+package javaconf.com.sayed.stereo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javaconf.com.sayed.stereo.soundsys.CDPlayerConfig;
+import auto.com.sayed.stereo.soundsys.CompactDisc;
+import auto.com.sayed.stereo.soundsys.MediaPlayer;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=CDPlayerConfig.class)
+public class JavaconfCDPlayerTest {
+
+  @Rule
+  public final SystemOutRule log = new SystemOutRule().enableLog();
+
+  @Autowired
+  private MediaPlayer player;
+  
+  @Autowired
+  private CompactDisc cd;
+  
+  @Test
+  public void cdShouldNotBeNull() {
+    assertNotNull(cd);
+  }
+
+  @Test
+  public void play() {
+    player.play();
+    assertEquals(
+        "Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\n", 
+        log.getLogWithNormalizedLineSeparator());
+  }
+
+}
+
